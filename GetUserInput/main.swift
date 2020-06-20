ea6fdc45932c2a797a3874cfefc27161d79618b8
SwiftCode <SwiftCode@gamil.com>
2020-06-20 11:12:46 +0800

    Initial Commit

---
+++ GetUserInput/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GetUserInput
+//
+//  Created by SwiftiCode on 20/6/20.
+//  Copyright © 2020 SwiftiCode. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
